06456cfff2342a43a231f01f0261e9d56622c20b
turtletown73 <turtletown73@gmail.com>
2024-03-02 14:23:43 -0500

    e

--- index.js
+++ index.js
@@ -4,51 +4,36 @@ const {spawn} = require("node:child_process")
 const app = express();
 const bodyParser = require('body-parser');
 
-app.use('/chat', express.static('chat/static'));
-app.use('/games', express.static('games/static'));
-app.use('/', express.static('main/static'));
+app.use('/chat', express.static('/home/scoobis/Documents/http-server/chat/static'));
+app.use('/games', express.static('/home/scoobis/Documents/http-server/games/static'));
+app.use('/', express.static('/home/scoobis/Documents/http-server/main/static'));
 app.use(bodyParser.urlencoded({extended: false}));
 
-spawn("cd games/backend/eaglercraft/server && sudo sh run_unix.sh", {shell: true});
-spawn("cd games/backend/eaglercraft/bungeecord && sudo sh run_unix.sh", {shell: true});
-spawn("cd games/backend/eaglercraft/relay && sudo sh run.sh", {shell: true});
+spawn("cd /home/scoobis/Documents/http-server/games/backend/eaglercraft/server && sudo sh run_unix.sh", {shell: true});
+spawn("cd /home/scoobis/Documents/http-server/games/backend/eaglercraft/bungeecord && sudo sh run_unix.sh", {shell: true});
+spawn("cd /home/scoobis/Documents/http-server/games/backend/eaglercraft/relay && sudo sh run.sh", {shell: true});
+spawn("sudo playit", {shell: true});
 
 app.post('/chat/text', (req, res) => {
     res.redirect("/chat");
 
-    fs.readFile("./chat/resources/aliases.txt", "utf8", function(err, data) {
-        ips = JSON.parse(data);
-        fs.readFile("./chat/resources/banlist.txt", "utf8", function(err, data) {
-            banlist = JSON.parse(data);
-            if (!banlist[req.ip.substring(7)] == 1) {
-                fs.readFile('./chat/messages/latestmsg.txt', 'utf8', function(err, data) {
-                    if ((ips[req.ip.substring(7)] == "Aidan" || req.ip.substring(7) == "127.0.0.1") && req.body.message.startsWith("/")) {
-                        if (req.body.message.substring(1).startsWith("say")) {
-                            fs.writeFile('./chat/messages/latestmsg.txt', (parseInt(data) - 1).toString(), function() {});
-                            fs.writeFile(`./chat/messages/msg${parseInt(data) - 1}.txt`, '[SERVER] ' + req.body.message.substring(5), function() {});
-                        }
-                    }
-                    else {
-                        fs.writeFile('./chat/messages/latestmsg.txt', (parseInt(data) - 1).toString(), function() {});
-                        if (ips[req.ip.substring(7)]) {
-                            fs.writeFile(`./chat/messages/msg${parseInt(data) - 1}.txt`, ips[req.ip.substring(7)] + ' says "' + req.body.message + '"', function() {});
-                        }
-                        else {
-                            fs.writeFile(`./chat/messages/msg${parseInt(data) - 1}.txt`, req.ip.substring(7) + ' says "' + req.body.message + '"', function() {});
-                        }
-                    }
-                });
-            }
-        });
+    fs.readFile("/home/scoobis/Documents/http-server/chat/resources/banlist.txt", "utf8", function(err, data) {
+        banlist = JSON.parse(data);
+        if (!banlist[req.ip.substring(7)] == 1) {
+            fs.readFile('/home/scoobis/Documents/http-server/chat/messages/latestmsg.txt', 'utf8', function(err, data) {
+                fs.writeFile('/home/scoobis/Documents/http-server/chat/messages/latestmsg.txt', (parseInt(data) + 1).toString(), function() {});
+                fs.writeFile(`/home/scoobis/Documents/http-server/chat/messages/msg${parseInt(data) + 1}.txt`, 'someone says "' + req.body.message + '"', function() {});
+            });
+        }
     });
 });
 
 app.get('/chat/text', (req, res) => {
     var text = [];
 
-    fs.readdir('./chat/messages', function(err, files) {
-        for (const i in files) {
-            fs.readFile(`./chat/messages/${files[i]}`, 'utf8', function(err,data) {
+    fs.readdir('/home/scoobis/Documents/http-server/chat/messages', function(err, files) {
+        for (const i in files.reverse()) {
+            fs.readFile(`/home/scoobis/Documents/http-server/chat/messages/${files[i]}`, 'utf8', function(err,data) {
                 if (files[i] != "latestmsg.txt") {
                     text.push(data);
                 }
@@ -61,12 +46,6 @@ app.get('/chat/text', (req, res) => {
     });
 });
 
-console.log("Games' backends running!")
-
 app.listen(80, () => {
-    console.log(`Website listening on port 80`);
-});
-
-app.listen(8080, () => {
-    console.log(`Website listening on port 8080`);
+    console.log(`Website running!`);
 });
